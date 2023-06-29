112516041583538d60ba6c6cf15762a323bade95
Igor <igoraraujobda@gmail.com>
2023-06-29 18:22:41 -0300

    implementando sessão superior com filtros dos webnars

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Header from '@/components/Organisms/Header';
 import GlobalStyles from '../../GlobalStyles';
 import Hero from '@/components/Organisms/Hero';
+import Webinars from '@/components/Organisms/Webnars';
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <GlobalStyles />
       <Header />
       <Hero />
+      <Webinars />
     </div>
   )
 }
